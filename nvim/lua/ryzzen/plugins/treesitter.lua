b2aa0231bbef3432582a36fe8ee327c7da891d8e
Ryzzen <a.mayaud@hotmail.fr>
2024-07-01 01:00:20 +0200

    [ADD](nvim) lazy

--- nvim/lua/ryzzen/plugins/treesitter.lua
+++ nvim/lua/ryzzen/plugins/treesitter.lua
@@ -1,17 +1,17 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  config = function()
-    require 'nvim-treesitter.configs'.setup {
-      ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query" },
+	"nvim-treesitter/nvim-treesitter",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "nix" },
 
-      highlight = {
-        enable = true,
-        disable = {}
-      },
-      indent = {
-        enable = true,
-        disable = {}
-      }
-    }
-  end,
+			highlight = {
+				enable = true,
+				disable = {},
+			},
+			indent = {
+				enable = true,
+				disable = {},
+			},
+		})
+	end,
 }
