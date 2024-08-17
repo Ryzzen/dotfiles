c46783de01867910c55b8bec9f7791c55ff17049
Ryzzen <a.mayaud@hotmail.fr>
2024-08-17 14:29:41 +0200

    [UPDATE](nvim) minimap

--- nvim/lua/ryzzen/plugins/right-tab.lua
+++ nvim/lua/ryzzen/plugins/right-tab.lua
@@ -1,11 +1,11 @@
 return {
-  "https://github.com/liuchengxu/vista.vim",
-  dependencies = {"wfxr/minimap.vim"},
-  config = function()
-    -- Keymaps
-    local keymap = vim.keymap 
+	"https://github.com/liuchengxu/vista.vim",
+	-- dependencies = { "wfxr/minimap.vim" },
+	config = function()
+		-- Keymaps
+		local keymap = vim.keymap
 
-    keymap.set("n", "<C-p>", ":MinimapClose<CR>:Vista!! <CR>", { desc = "Open vista" })
-    keymap.set("n", "<C-m>", ":MinimapToggle<CR>", { desc = "Open minimap" })
-  end
+		keymap.set("n", "<C-p>", ":MinimapClose<CR>:Vista!! <CR>", { desc = "Open vista" })
+		-- keymap.set("n", "<C-m>", ":MinimapToggle<CR>", { desc = "Open minimap" })
+	end,
 }
