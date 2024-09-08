6b9319522cfe66b912a54ca179743297f02a79bb
Ryzzen <a.mayaud@hotmail.fr>
2024-09-08 20:48:17 +0200

    [UPDATE](nvim) pwn template update

--- nvim/templates/pwn_template.py
+++ nvim/templates/pwn_template.py
@@ -9,21 +9,21 @@ exe = context.binary = ELF(args.EXE or "./NAME")
 
 
 def start(argv=[], *a, **kw):
-    if args.GDB:
-        return gdb.debug([exe.path] + argv, gdbscript=gdbscript, *a, **kw)
-    elif args.SSH:
+    if args.SSH:
         s = ssh(
-            user="root",
-            host="172.22.0.2",
-            port=22,
+            user="poun",
+            host="127.0.0.1",
+            port=2222,
             password="poun",
         )
         if args.GDB:
-            return gdb.debug(exe.path, exe="NAME", ssh=s, gdbscript=gdbscript)
+            return gdb.debug("NAME", ssh=s, gdbscript=gdbscript)
         else:
             return s.system("NAME")
     elif args.REM:
         return remote(host="127.0.0.1", port=8080)
+    elif args.GDB:
+        return gdb.debug([exe.path] + argv, gdbscript=gdbscript, *a, **kw)
     else:
         return process([exe.path] + argv, *a, **kw)
 
