83c87c5a805db628996330307e3cf9e51f1e39bb
MAYAUD Antoine <a.mayaud@serma.com>
2025-07-10 17:38:29 +0200

    [UPDATE](keymaps) lua snip jump

--- nvim/lua/ryzzen/plugins/cmp.lua
+++ nvim/lua/ryzzen/plugins/cmp.lua
@@ -67,8 +67,6 @@ return {
 		vim.keymap.set({ "i", "s" }, "<Tab>", function()
 			if luasnip.jumpable(1) then
 				luasnip.jump(1)
-			else
-				vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", true)
 			end
 		end, { silent = true })
 
@@ -76,8 +74,6 @@ return {
 		vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
 			if luasnip.jumpable(-1) then
 				luasnip.jump(-1)
-			else
-				vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<S-Tab>", true, false, true), "n", true)
 			end
 		end, { silent = true })
 	end,
